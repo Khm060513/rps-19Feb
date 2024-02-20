9c76062cb26b1da569aea08a9c7f4644a59eccd5
Khm060513 <160450712+Khm060513@users.noreply.github.com>
2024-02-20 21:49:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,8 @@ def image_gpt():
 
 @app.route("/end",methods=["GET","POST"])
 def end():
-  first_time = 1
+  global first_time 
+  first time = 1
   return(render_template("end.html"))
 
 if __name__=="__main__":
